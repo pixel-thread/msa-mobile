eb6504bb89068ad35b7a0f0ccb95dbf574bd1805
JyrwaJR <jyrwaboys@gmail.com>
2026-05-12 22:56:53 +0530

    feat: add useDeleteMeetingMinute hook

--- src/features/meetings/hooks/index.ts
+++ src/features/meetings/hooks/index.ts
@@ -3,3 +3,5 @@ export { useMeeting } from './use-meeting';
 export { useMeetingAgenda } from './useMeetingAgenda';
 export { useMeetingMinuite } from './use-meeting-minuite';
 export { useCreateMeetingMinuite } from './use-create-meeting-minuite';
+export { useDeleteMeetingMinute } from './use-delete-meeting-minute';
+export { useUpdateMeetingMinuite } from './use-update-meeting-minuites';
---
+++ src/features/meetings/hooks/use-delete-meeting-minute.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import http from '@src/shared/utils/http';
+import { toast } from 'sonner-native';
+
+type Props = {
+  meetingId: string;
+};
+
+export function useDeleteMeetingMinute({ meetingId }: Props) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (meetingMinuteId: string) =>
+      http.delete(`/meeting/${meetingId}/minuite/${meetingMinuteId}`),
+    onSuccess: (data) => {
+      if (data.success) {
+        toast.success(data.message || 'Minute deleted successfully');
+        queryClient.invalidateQueries({ queryKey: ['meeting', 'minuites', meetingId] });
+        return data;
+      }
+      toast.error(data.message || 'Failed to delete minute');
+      return data;
+    },
+  });
+}

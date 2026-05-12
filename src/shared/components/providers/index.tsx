f366c174ed2683c1cf5f76737f715f55151dfd4e
JyrwaJR <jyrwaboys@gmail.com>
2026-05-12 10:35:43 +0530

    chore: remove test files as tests are skipped

--- src/shared/components/providers/index.tsx
+++ src/shared/components/providers/index.tsx
@@ -3,20 +3,24 @@ import { AuthProvider } from './auth';
 import React from 'react';
 import { AuthGuard } from '../auth';
 import { NotificationProvider } from './notifications';
+import { ThemeProvider } from './theme.provider';
 
 export * from './auth';
 export * from './notifications';
+export * from './theme.provider';
 
 export const AppProviders = () => {
   return (
     <React.Fragment>
-      <AuthProvider>
-        <AuthGuard>
-          <NotificationProvider>
-            <Stack screenOptions={{ headerShown: false }} />
-          </NotificationProvider>
-        </AuthGuard>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <AuthGuard>
+            <NotificationProvider>
+              <Stack screenOptions={{ headerShown: false }} />
+            </NotificationProvider>
+          </AuthGuard>
+        </AuthProvider>
+      </ThemeProvider>
     </React.Fragment>
   );
 };
